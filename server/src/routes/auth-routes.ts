0932b4585f9dc5a105fbc02b9969f96797b17540
Codergeek081 <issefarah15@gmail.com>
2025-04-01 12:00:37 -0400

    Changes to authentication

---
+++ server/src/routes/auth-routes.ts
@@ -0,0 +1,29 @@
+import { Router, Request, Response } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+export const login = async (req: Request, res: Response) => {
+  // TODO: If the user exists and the password is correct, return a JWT token
+
+  // how jwt works - chatgpt
+  // search if user exists in the database using req.body {username, password}
+  // if user exits - return jwt token
+  // if users doesnt exits you retrun 401 
+  // import jwt from 'jsonwebtoken'; - how to generate jwt token
+  // on frontend - if user exits - rediredct to right page
+  // if user not exits - show error
+  // in your api on routes that return sensitive information - apply jwt check / valid
+  // up: protect routes with jwt!
+  
+
+
+  return res.json(req.body)
+};
+
+const router = Router();
+
+// POST /login - Login a user
+router.post('/login', login);
+
+export default router;
