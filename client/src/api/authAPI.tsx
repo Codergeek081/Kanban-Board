ebe544906c3d0338b27fc7040d237dfe68562397
Codergeek081 <issefarah15@gmail.com>
2025-04-26 11:59:32 -0400

    fix

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -7,7 +7,7 @@ const login = async (userInfo: UserLogin) => {
     // Debugging: Log the user info being sent
     console.log("Sending login date", userInfo);
     // Making a POST request to the login route
-    const response = await fetch('http://localhost:3001/auth/login', {
+    const response = await fetch('https://kanban-board-1-05q9.onrender.com/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=utf-8'
